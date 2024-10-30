3eddbdd449a8047f21078427ea9b2d3315306273
Matt861 <mattwindham21@gmail.com>
2024-10-29 19:38:35 -0500

    restructured, added common.gradle file

--- src/main/java/com/lmco/crt/Main.java
+++ src/main/java/com/lmco/crt/Main.java
@@ -1,59 +1,59 @@
 package com.lmco.crt;
 
-import com.fasterxml.jackson.annotation.SimpleObjectIdResolver;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.annotation.SimpleObjectIdResolver;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
-        com.fasterxml.jackson.annotation.SimpleObjectIdResolver xx = new SimpleObjectIdResolver();
-        xx.bindItem(null, null);
-        mainTest();
+//        com.fasterxml.jackson.annotation.SimpleObjectIdResolver xx = new SimpleObjectIdResolver();
+//        xx.bindItem(null, null);
+//        mainTest();
     }
 
-    public static void mainTest() {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            // Create an example object
-            MyClass myObject = new MyClass("example", 123);
-
-            // Serialize to JSON
-            String jsonString = objectMapper.writeValueAsString(myObject);
-            System.out.println("Serialized JSON: " + jsonString);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static class MyClass {
-        private String name;
-        private int value;
-
-        // Constructors, getters, and setters
-        public MyClass() {}
-
-        public MyClass(String name, int value) {
-            this.name = name;
-            this.value = value;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-    }
+//    public static void mainTest() {
+//        try {
+//            ObjectMapper objectMapper = new ObjectMapper();
+//
+//            // Create an example object
+//            MyClass myObject = new MyClass("example", 123);
+//
+//            // Serialize to JSON
+//            String jsonString = objectMapper.writeValueAsString(myObject);
+//            System.out.println("Serialized JSON: " + jsonString);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    static class MyClass {
+//        private String name;
+//        private int value;
+//
+//        // Constructors, getters, and setters
+//        public MyClass() {}
+//
+//        public MyClass(String name, int value) {
+//            this.name = name;
+//            this.value = value;
+//        }
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        public int getValue() {
+//            return value;
+//        }
+//
+//        public void setValue(int value) {
+//            this.value = value;
+//        }
+//    }
 }
